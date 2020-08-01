8cd0e0efe8306262321b7c88036e2b376a15b06c
zejiran <juanszalegria@gmail.com>
2020-08-01 14:51:27 -0500

    docs: endpoint parameters and responses
    Add documentation including parameters and responses of GET request
    for buyers, products and transactions.

--- api-rest/main.go
+++ api-rest/main.go
@@ -8,9 +8,18 @@ import (
 )
 
 func main() {
+    fmt.Println("Starting the application...")
     port := ":3000"
     r := chi.NewRouter()
-    // GET request for buyers
+    /* GET request for buyers
+     Parameters:
+        - date: date to query on unix timestamp format
+     Response:
+        Format: JSON
+        - id: Buyer ID
+        - name: Buyer name
+        - age: Buyer age
+    */
     r.Get("/buyers", func(w http.ResponseWriter, r *http.Request) {
         url_buyers := "https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers"
         response, err := http.Get(url_buyers)
@@ -21,7 +30,15 @@ func main() {
             w.Write([]byte(string(data)))
         }
     })
-    // GET request for products
+    /* GET request for products
+     Parameters:
+        - date: date to query on unix timestamp format
+     Response:
+        Format: CSV separed by '
+        - id: Product ID
+        - name: Product name
+        - price: Price in dollar cents
+    */
     r.Get("/products", func(w http.ResponseWriter, r *http.Request) {
         url_products := "https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/products"
         response, err := http.Get(url_products)
@@ -32,7 +49,17 @@ func main() {
             w.Write([]byte(string(data)))
         }
     })
-    // GET request for transactions
+    /* GET request for transactions
+     Parameters:
+        - date: date to query on unix timestamp format
+     Response:
+        Format: No standard
+        - id: Transaction ID
+        - buyer id: Buyer ID
+        - ip: Buyer IP
+        - device: Buyer devices (ios, android, windows, linux, mac)
+        - product ids: List of purchased products
+    */
     r.Get("/transactions", func(w http.ResponseWriter, r *http.Request) {
         url_transactions := "https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/transactions"
         response, err := http.Get(url_transactions)
